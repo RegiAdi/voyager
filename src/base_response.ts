c853059d569014ce81c5d2b08f17e70f75386cf1
Regi Adi <regiadi.priyanto@gmail.com>
2023-12-21 12:29:45 +0700

    Add BaseResponse

---
+++ src/base_response.ts
@@ -0,0 +1,21 @@
+import {Response} from 'express';
+
+export class BaseResponse {
+  send(
+    res: Response,
+    statusCode: number,
+    status: string,
+    message: string,
+    data: object | Array<object>
+  ): void {
+    res.status(statusCode).json({
+      status: status,
+      message: message,
+      data: data,
+    });
+  }
+
+  run(): void {
+    console.log('BaseResponse.run()');
+  }
+}
--- src/user_handler.ts
+++ src/user_handler.ts
@@ -1,8 +1,16 @@
 import {Request, Response} from 'express';
+import {BaseResponse} from './base_response';
 
 export class UserHandler {
-  getOneUser(req: Request, res: Response): void {
-    res.send('Voyager /users/1');
+  constructor() {}
+
+  getOneUser(req: Request, res: Response) {
     console.log('GET /users/1');
+
+    const baseResponse = new BaseResponse();
+    baseResponse.send(res, 200, 'SUCCESS', 'User retrieved sucessfully', {
+      id: 1,
+      name: 'user1',
+    });
   }
 }
