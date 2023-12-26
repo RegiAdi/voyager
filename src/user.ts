65a84798b7ffde41cc4c4afbb8b232b1445c0bc1
Regi Adi <regiadi.priyanto@gmail.com>
2023-12-26 11:07:28 +0700

    Add User model

---
+++ src/user.ts
@@ -0,0 +1,15 @@
+interface User {
+  usercode: string;
+  username: string;
+  password: string;
+  email: string;
+  fullname: string;
+  phone: string;
+  address: string;
+  apiToken: string;
+  deviceName: string;
+  tokenLastUsedAt: string;
+  tokenExpiresAt: string;
+  createdAt: string;
+  updatedAt: string;
+}
